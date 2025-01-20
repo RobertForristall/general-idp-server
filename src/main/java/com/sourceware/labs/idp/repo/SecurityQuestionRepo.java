657bc477ab2ac4d444d9f06a9a005bd571c0a1ff
Robert <robert.s.forristall@gmail.com>
2025-01-20 16:22:32 -0500

    feat: update security question repo to find by user

--- src/main/java/com/sourceware/labs/idp/repo/SecurityQuestionRepo.java
+++ src/main/java/com/sourceware/labs/idp/repo/SecurityQuestionRepo.java
@@ -1,9 +1,12 @@
 package com.sourceware.labs.idp.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sourceware.labs.idp.entity.SecurityQuestion;
+import com.sourceware.labs.idp.entity.User;
 
 public interface SecurityQuestionRepo extends JpaRepository<SecurityQuestion, Long> {
-
+  List<SecurityQuestion> findSecurityQuestionByUser(User user);
 }
