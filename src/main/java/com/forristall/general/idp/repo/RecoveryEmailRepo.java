4f357ce20a14739b46c7334696f7da761fde230d
Robert <robert.s.forristall@gmail.com>
2025-11-12 23:03:06 -0500

    feat: add finding recovery email using email in recovery email repo

--- src/main/java/com/forristall/general/idp/repo/RecoveryEmailRepo.java
+++ src/main/java/com/forristall/general/idp/repo/RecoveryEmailRepo.java
@@ -1,9 +1,13 @@
 package com.forristall.general.idp.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.forristall.general.idp.entity.RecoveryEmail;
 
 public interface RecoveryEmailRepo extends JpaRepository<RecoveryEmail, Long> {
 
+  List<RecoveryEmail> findRecoveryEmailByEmail(String email);
+  
 }
