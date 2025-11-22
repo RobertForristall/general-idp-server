90aed7c41f3b1e02d65f1205e83b8c9787fdb53a
Robert <robert.s.forristall@gmail.com>
2025-11-21 21:45:17 -0500

    feat: add finding recovery code by code to recovery code repo

--- src/main/java/com/forristall/general/idp/repo/RecoveryCodeRepo.java
+++ src/main/java/com/forristall/general/idp/repo/RecoveryCodeRepo.java
@@ -1,9 +1,13 @@
 package com.forristall.general.idp.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.forristall.general.idp.entity.RecoveryCode;
 
 public interface RecoveryCodeRepo extends JpaRepository<RecoveryCode, Long> {
 
+  List<RecoveryCode> findRecoveryCodeByCode(String code);
+  
 }
